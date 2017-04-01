f8cd3d485e8bdd716baf08895e40af0cff49d249
Eka Rudianto <ekarudianto@yahoo.com>
2017-04-01 22:42:38 +0800

    Sort

--- src/main/java/com/ekarudianto/repository/UserRepository.java
+++ src/main/java/com/ekarudianto/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.ekarudianto.repository;
 
+import com.ekarudianto.model.User;
 import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.ekarudianto.model.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
 	public List<User> findAll();
--- src/main/java/com/ekarudianto/rest/UsersController.java
+++ src/main/java/com/ekarudianto/rest/UsersController.java
@@ -1,14 +1,12 @@
 package com.ekarudianto.rest;
 
+import com.ekarudianto.model.User;
+import com.ekarudianto.repository.UserRepository;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.ekarudianto.model.User;
-import com.ekarudianto.repository.UserRepository;
 
 @RestController
 @RequestMapping(value = "/users")
